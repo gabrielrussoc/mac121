5ec07fc9ffd679754098feaf42d07cea686edd40
Gabriel Russo <gabrielrcarmo@gmail.com>
2015-08-20 21:10:31 -0300

    arrumando tab

--- tarefas/C/GABRIELD/pisolog.c
+++ tarefas/C/GABRIELD/pisolog.c
@@ -29,24 +29,24 @@
 // de n na base 2. Faz isso contando quantas vezes o numero pode
 // ser divido por 2. */
 int piso_log (int n) {
-	int ans = 0;
-	while (n > 1) {
-		/* n <= (b^k)/(2^i), pois eh sempre o piso*/
-		n /= 2;
-		ans++;
-	}
-	return ans;
+    int ans = 0;
+    while (n > 1) {
+        /* n <= (b^k)/(2^i), pois eh sempre o piso*/
+        n /= 2;
+        ans++;
+    }
+    return ans;
 }
 
 int main (int argc, char *argv[]) {
-	int b, k, i, num;
-	b = num = atoi (argv[1]);
-	k = atoi (argv[2]);
-	printf("         N | |_lg(N)_|\n\n"); 
-	for (i = 0; i < k; i++) {
-		printf ("%10d | %5d\n", num, piso_log (num)); 
-		if(num >= INT_MAX / b) break;
-		num *= b;
-	}
-	return EXIT_SUCCESS;	
+    int b, k, i, num;
+    b = num = atoi (argv[1]);
+    k = atoi (argv[2]);
+    printf("         N | |_lg(N)_|\n\n"); 
+    for (i = 0; i < k; i++) {
+        printf ("%10d | %5d\n", num, piso_log (num)); 
+        if(num >= INT_MAX / b) break;
+        num *= b;
+    }
+    return EXIT_SUCCESS;
 }
